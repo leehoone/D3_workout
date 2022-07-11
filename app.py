d95be5ee690c1a5bd0092de0ecaac2c1b778eb14
ji-0o0o0o <tlswldud28@naver.com>
2022-07-11 17:04:09 +0900

    로그인 서버 작성

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.hmskj9c.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/api/login', methods=["POST"])
+def D3_workout_post():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    doc = {
+        'id': id_receive,
+        'pw': pw_receive
+    }
+    db.login.insert_one(doc)
+
+    return jsonify({'msg':'로그인 완료!'})
